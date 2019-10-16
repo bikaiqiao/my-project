838b5c96775bbd59fc66e5d0c3725bfd3e1d697c
bikaiqiao <48797189+bikaiqiao@users.noreply.github.com>
2019-10-16 22:48:19 +0800

    修改冗余代码提交静态文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Home from '@/pages/home'
-import Detail from '@/pages/detail'
 
 Vue.use(Router)
 
